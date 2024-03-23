a679cc5e833b25f515610252555436a54c616f4f
roose7392 <roose7392@gmail.com>
2024-03-23 15:31:49 +0500

    finish game

--- main.js
+++ main.js
@@ -22,20 +22,8 @@ function GameBoard() {
   }
 
   const getBoard = () => board;
-  const printBoard = () => { 
-    console.log('__________')
-    board.forEach(row => {
 
-    console.log(row.reduce((acc, curr) => {
-      if(curr.getValue() === null) {
-        return acc + ' . '
-      }
-      return acc + ` ${curr.getValue()} ` 
-    }, ''))})
-    console.log('__________')
-}
-
-  return { getBoard, printBoard, markCell};
+  return { getBoard, markCell};
 };
 
 function Cell() {
@@ -61,14 +49,16 @@ function Player(name, mark) {
 }
 
 function GameController(playerOne, playerTwo) {
+
   const gameData = {
     isOver: false,
     winner: null,
     winningCombination: null
   };
+
   const players = [playerOne, playerTwo];
   let activePlayer = players[0]
-  const {getBoard, printBoard, markCell} = GameBoard()
+  const {getBoard, markCell} = GameBoard()
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -88,20 +78,19 @@ function GameController(playerOne, playerTwo) {
     if(!roundValid) {
       return
     }
-
     changeActivePlayer()
-    printBoard()
-
     return true
   }
 
-  const checkWinner = () => {
-    let isOver = false
+  const checkStatus = () => {
+    let isOver = 'ongoing'
     const flatValues = getBoard().flat().map(cell => cell.getValue())
     winningCombinations.forEach(combo => {
       const check = combo.map(num => flatValues[num])
       if(check[0] !== null && check.every(num => num === check[0])) {
-        isOver = true
+        isOver = 'victory'
+      } else if(flatValues.every(value => value!== null)) {
+        isOver = isOver === 'victory' ? 'victory' : 'draw'
       }
     })
     return isOver
@@ -113,20 +102,45 @@ function GameController(playerOne, playerTwo) {
     playRound,
     getGameData,
     getActivePlayer,
-    checkWinner
+    checkStatus
   }
 }
 
-function DisplayController() {
+function DisplayController(counter = 0,playerOneName, playerTwoName) {
+  document.querySelector('#app').innerHTML = 
+  `<p class="turn">Player one's turn</p>
+  <div class="board">
+    <div class="cell" data-row="0" data-column="0"></div>
+    <div class="cell" data-row="0" data-column="1"></div>
+    <div class="cell" data-row="0" data-column="2"></div>
+    <div class="cell" data-row="1" data-column="0"></div>
+    <div class="cell" data-row="1" data-column="1"></div>
+    <div class="cell" data-row="1" data-column="2"></div>
+    <div class="cell" data-row="2" data-column="0"></div>
+    <div class="cell" data-row="2" data-column="1"></div>
+    <div class="cell" data-row="2" data-column="2"></div>
+</div>
+<button class="restart-button hidden">Restart</button>`
 
+  while(!playerOneName) {
+    playerOneName = prompt('Enter Player One Name')
+  }
+
+  while(!playerTwoName) {
+    playerTwoName = prompt('Enter Player Two Name')
+  }
+  const players = counter % 2 === 0 
+  ? [Player(playerOneName, 'X'), Player(playerTwoName, 'O')]
+  : [Player(playerTwoName, 'O'), Player(playerOneName, 'X')]
   const controller = GameController(
-    Player('Player One', 'X'),
-    Player('Player Two', 'O')
+    ...players
   );
 
+  const cells = document.querySelectorAll('.cell');
+  const display = document.querySelector('.turn')
+  const restartButton = document.querySelector('.restart-button')
   const board = document.querySelector('.board');
-  board.addEventListener('click', e => {
-
+  const boardListener = e => {
     if(e.target === board) {
       return
     }
@@ -138,22 +152,43 @@ function DisplayController() {
     if(!isValid) {
       return
     } 
-    const gameOver = controller.checkWinner()
-    if(gameOver) {
-      alert(`game over, ${currentPlayer.getName()} won`)
-    }
+    const gameStatus = controller.checkStatus()
     e.target.classList.add('marked')
     e.target.innerText = mark
-    setHover()
+    updateDisplay(controller.getActivePlayer())
+    if(gameStatus === 'victory') {
+      endGame()
+      display.innerText = `${currentPlayer.getName()} won!`
 
-  })
-  
-  const setHover = () => {
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => cell.setAttribute('turn', controller.getActivePlayer().getMark()))
+    }
+    if(gameStatus === 'draw') {
+      endGame()
+      display.innerText = 'Draw!'
+    }
+
+    function endGame() {
+      board.removeEventListener('click', boardListener)
+      cells.forEach(cell => cell.removeAttribute('turn'))
+      toggleRestartButton();
   }
 
-  setHover()
+    
+  }
+  board.addEventListener('click', boardListener)
+  
+
+  const updateDisplay = (player) => {
+    cells.forEach(cell => cell.setAttribute('turn', player.getMark()));
+    display.innerText = `${player.getName()}'s turn`
+  }
+
+  updateDisplay(controller.getActivePlayer());
+
+  function toggleRestartButton() {
+    restartButton.classList.toggle('hidden')
+  }
+
+  restartButton.addEventListener('click', () => DisplayController(++counter, playerOneName, playerTwoName))
 }
 
 DisplayController()
